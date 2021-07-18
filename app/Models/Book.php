5d85dec39702081e148e2ed6fd925a1dc7b38217
Carlos Panganiban <cgpanganiban@up.edu.ph>
2021-07-18 13:39:06 +0800

    Add models

---
+++ app/Models/Book.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Book extends Model
+{
+    use HasFactory;
+
+    protected $table = "books";
+
+    public function chapters() {
+        return $this->hasMany(Chapter::class);
+    }
+}
---
+++ database/factories/ChapterStatusFactory.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\ChapterStatus;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class ChapterStatusFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = ChapterStatus::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            //
+        ];
+    }
+}
